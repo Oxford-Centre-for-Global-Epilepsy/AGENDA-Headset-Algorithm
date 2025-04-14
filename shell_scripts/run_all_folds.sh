0d72496944bce8aa3bcca4a5aa7c47ad7e29cfa5
John E Fleming <jfleming9317@gmail.com>
2025-04-14 19:44:34 +0100

    Updated run_all_folds.sh so that number of folds and model pooling type can be specified from here

--- shell_scripts/run_all_folds.sh
+++ shell_scripts/run_all_folds.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
-NUM_FOLDS=5
-POOLING_TYPE="mean"      # mean, attention or transformer
+# Default values
+DEFAULT_NUM_FOLDS=5
+DEFAULT_POOLING_TYPE="mean"
 
+# Read input parameters or use defaults
+NUM_FOLDS=${5:-$DEFAULT_NUM_FOLDS}
+POOLING_TYPE=${2:-$DEFAULT_POOLING_TYPE}
+
+# Create jobs
 for FOLD in $(seq 0 $((NUM_FOLDS - 1))); do
   bash shell_scripts/run_fold.sh $FOLD $POOLING_TYPE
 done
